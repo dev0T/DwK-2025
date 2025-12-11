1802a7baf07a9cd878f1b7771d81f5111d5a84dd
Teryn Alves <28127196+dev0T@users.noreply.github.com>
2025-12-11 11:55:20 -0800

    todo-app: updated dependencies and added dynamic base path

--- the_project/todo-app/next.config.ts
+++ the_project/todo-app/next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
+const envBasePath = (): string => {
+  const currentEnv = process.env.ENV;
+  if (currentEnv) {
+      switch (currentEnv) {
+    case "production":
+      return "";
+    default:
+      return currentEnv
+    }
+  }
+  return ""
+}
+
 const nextConfig: NextConfig = {
+  basePath: envBasePath(),
   output: "standalone",
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+    incomingRequests: {
+      ignore: [/\api\/v1\/health/],
+    }
+  }
 };
 
 export default nextConfig;
