c1ca5a26f292bc4e97b3272d60c312d18b3cf0c4
Teryn Alves <28127196+dev0T@users.noreply.github.com>
2025-12-17 16:55:14 -0800

    todo-app: updated Dockerfile and removed unecessary assetPrefix

--- the_project/todo-app/next.config.ts
+++ the_project/todo-app/next.config.ts
@@ -3,19 +3,18 @@ import type { NextConfig } from "next";
 const envBasePath = (): string => {
   const currentEnv = process.env.ENV;
   if (currentEnv) {
-      switch (currentEnv) {
-    case "production":
-      return "";
-    default:
-          return `/${currentEnv}`
-    }
+    switch (currentEnv) {
+      case "production":
+        return "";
+      default:
+        return `/${currentEnv}`
+      }
   }
   return ""
 }
 
 const nextConfig: NextConfig = {
   basePath: envBasePath(),
-  assetPrefix: envBasePath(),
   output: "standalone",
   serverExternalPackages: ['pino', 'next-logger'],
   logging: {
